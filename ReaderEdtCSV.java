6d529a538f517cc845ff192d7182590772261bd0
Elbazibaz <eb.adrian@hotmail.fr>
2017-12-14 22:09:23 +0100

    1st commit

---
+++ ReaderEdtCSV.java
@@ -0,0 +1,59 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Path;
+
+public class ReaderEdtCSV {
+	
+	String line;
+	String sep;
+	String edt;
+	String etu;
+	
+	public ReaderEdtCSV() {
+		line=null;
+		sep=";";
+		edt="edt.csv";
+		etu="etus.csv";
+	}
+	
+	public ReaderEdtCSV(String s1,String s2) {
+		line=null;
+		sep=";";
+		edt=s1;
+		etu=s2;
+	}
+	public void read() {
+		String[] tab;
+		int compteur=0;
+		try {
+			BufferedReader in= new BufferedReader(new FileReader(this.edt));
+			
+			line=in.readLine();
+			while(line!=null) {
+				tab=line.split(";");
+				if (compteur==0);
+				
+				
+				line=in.readLine();			}
+			//appel objet constructeur etudiant
+			// On peut retourner une liste d'etudiants et une structure modélisant l
+			//l'emploi du temps
+			
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Le fichier n'existe pas !");
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ main.java
@@ -0,0 +1,43 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int p=5;
+		System.out.println("Voici les options disponibles :");
+		System.out.println("################");
+		System.out.println("0-Quitter.");
+		System.out.println("################");
+		System.out.println("1-Lecture des fichiers par défaut.");
+		System.out.println("################");
+		System.out.println("2-Lecture de fichier spécifique.");
+		System.out.println("################");
+		System.out.println("3-Répartir les étudiants dans les TD.");
+		System.out.println("################");
+		System.out.println("4-Changer la capacité des TD.");
+		System.out.println("################");
+		System.out.println("################");
+		
+		while(p!=0) {
+			
+			
+			switch (p) {
+			case 1 :
+			
+				break;
+			case 2 :
+				
+				break;
+			case 3 :
+				
+				break;
+			case 4 :
+				
+				break;
+				
+			}
+			
+		}
+	}
+
+}
